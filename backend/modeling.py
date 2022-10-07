e8c97733fe28c704a304daf6916623b0dcc33ac2
Alex Kan <lexokan@gmail.com>
2022-10-06 21:31:13 -0400

    Save model plots

--- backend/modeling.py
+++ backend/modeling.py
@@ -1,11 +1,12 @@
+import pickle
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import joblib
 
-from sklearn.neighbors import NearestNeighbors
 from sklearn.linear_model import LinearRegression, LogisticRegressionCV
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve
+from sklearn.metrics import RocCurveDisplay, PrecisionRecallDisplay
 
 GLOBAL_RANDOM_STATE = 0
 GLOBAL_TEST_SIZE = .20
@@ -45,7 +46,24 @@ print(f"Linear Regression Equation\n: {clf.intercept_} + \
 print(clf.predict(np.array([[2, 2]])))
 print(clf.predict(np.array([[4, 2]])))
 
+y_pred = clf.predict(X_test)
 joblib.dump(clf, f"models/bed_bath_regressor.pkl")
+
+# Create a 3d plot showing the predicted plane of the 2d regression
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(X_test[:, 0], X_test[:, 1], y_test)
+
+X, Y = np.meshgrid(X_test[:, 0], X_test[:, 1])
+Z = clf.intercept_ + clf.coef_[0] * X + clf.coef_[1] * Y
+
+ax.plot_surface(X, Y, Z, color='r', alpha=0.5)
+ax.set_xlabel('Number of Bedrooms')
+ax.set_ylabel('Number of Bathrooms')
+ax.set_zlabel('Home Price')
+
+# Save the image as a pickle file for consumption in the frontend
+pickle.dump(fig, open('images/3dplot.pickle', 'wb'))
 
 """ Train a second model predicting price given # of bed/bathrooms and sqft """
 
@@ -78,6 +96,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 clf_3 = LogisticRegressionCV(cv=5, random_state=GLOBAL_RANDOM_STATE)
 clf_3.fit(X_train, y_train)
 joblib.dump(clf_3, f"models/logreg.pkl")
+
+# Save the ROC and PR curves for the logreg model
+RocCurveDisplay.from_estimator(clf_3, X_test, y_test)
+plt.savefig('images/roccurve.png')
+
+PrecisionRecallDisplay.from_estimator(clf_3, X_test, y_test)
+plt.savefig('images/prcurve.png')
 
 logreg_probabilities = clf_3.predict_proba(np.array([[2, 2, 1950]]))
 print(f"\nLogreg Predictions:\n")
